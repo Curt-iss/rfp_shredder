2a4d83a6ade73f27c5a44ff763780f5bf1413209
Tanner Spicerr <tanner9@vt.edu>
2020-02-13 12:35:13 -0500

    Added skeleton script

---
+++ scripts/pdf_to_docx.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+# Imports ---------------------------------------------------------------------
+
+# import sys
+
+# Functions -------------------------------------------------------------------
+
+# Main ------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    # Use sys.argv to get a list of command line arguments
+    # If you execute the scripts like: python3 pdf_to_docx.py some/file/path
+    #
+    # Then sys.argv[1] is the string 'some/file/path'
+    #
+    # FYI: sys.argv[0] is always the name of the script file, so 'pdf_to_docx.py'
+    pass
