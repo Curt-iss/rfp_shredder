13798b4e36c22fc7482c8abf885100029919a6f2
Curt-iss <tanner9@vt.edu>
2020-02-19 10:21:33 -0500

    minor command changes

--- scripts/pdf_to_docx.py
+++ scripts/pdf_to_docx.py
@@ -17,17 +17,18 @@ if __name__ == '__main__':
     #
     # Then sys.argv[1] is the string 'some/file/path'
     #
-    # FYI: sys.argv[0] is always the name of the script file, so 'pdf_to_docx.py'
+    # FYI: sys.argv[0] is always the name of the script file, so
+    # 'pdf_to_docx.py'
     if len(sys.argv) > 1:
         pdf_path = Path(sys.argv[1])
-        
-        #Check the file extension for 'pdf'
+
+        # Check the file extension for 'pdf'
         if pdf_path.suffix == '.pdf':
-            # Format the command 
-            command_str = f'lowriter --invisible --convert-to docx "{pdf_path}"'
+            # Format the command
+            command_str = f'soffice --headless --convert-to docx:"MS Word 2007 XML" "{pdf_path}"'
 
             # Make a call to libreoffice
-            subprocess.call(command_str, shell = True)
+            subprocess.call(command_str, shell=True)
 
         else:
             print('Please give me a path to a pdf...')
