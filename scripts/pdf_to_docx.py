1fda7f79209faa3a3b588a98acdaa3576f47d5c1
Curt-iss <tanner9@vt.edu>
2020-02-19 09:43:18 -0500

    filled in pdf_to_docx

--- scripts/pdf_to_docx.py
+++ scripts/pdf_to_docx.py
@@ -3,7 +3,9 @@
 
 # Imports ---------------------------------------------------------------------
 
-# import sys
+from pathlib import Path
+import subprocess
+import sys
 
 # Functions -------------------------------------------------------------------
 
@@ -16,4 +18,20 @@ if __name__ == '__main__':
     # Then sys.argv[1] is the string 'some/file/path'
     #
     # FYI: sys.argv[0] is always the name of the script file, so 'pdf_to_docx.py'
-    pass
+    if len(sys.argv) > 1:
+        pdf_path = Path(sys.argv[1])
+        
+        #Check the file extension for 'pdf'
+        if pdf_path.suffix == '.pdf':
+            # Format the command 
+            command_str = f'lowriter --invisible --convert-to docx "{pdf_path}"'
+
+            # Make a call to libreoffice
+            subprocess.call(command_str, shell = True)
+
+        else:
+            print('Please give me a path to a pdf...')
+            sys.exit(1)
+    else:
+        print('Please give me a path to a pdf...')
+        sys.exit(1)
