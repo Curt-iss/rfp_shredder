791f6d2877035bc14cd39e5a8c362cb6c9fb7194
Curt-iss <tanner9@vt.edu>
2020-03-05 12:45:58 -0500

    added debug commet

--- scripts/highlight_search_terms.py
+++ scripts/highlight_search_terms.py
@@ -13,13 +13,31 @@ for block in iter_block_items(doc):
     print(block.text)
 '''
 
+# How To Debug Crap in VSCode:
+#
+#
+# 1. Somewhere in the file you want to debug, find a line you want to "stop" on
+#       (i.e. set a breakpoint), at the time of writing, scroll down to line __
+#       If you mouse over the line number on the left side of the editor, you'll
+#       see a red dot. Click the dot to set a breakpoint.
+# 2. Find the Ladybug with a play button on the the Left side of your window
+#       Click on it
+# 3. You should see a button that says "Run an Debug Python", click it or press F5
+# 4. You'll see a popup in the middle of the screen, you want the first "Python File (Debug current file)"
+# 5. Give it a sec and the editor should jump to line you set the breakpoint on. You can move forward
+#       line by line using the buttons in the upper left
+#
+# You're gonna wanna read this:
+# https://code.visualstudio.com/docs/python/debugging
+# Some of the pictures are a little dated
+
 # Functions -------------------------------------------------------------------
 
 # Pass in the file name to open:
 def highlight_terms(file_name):
     """ Highlight some search terms in a given file.
 
-        Blah blah blah example docstring. This is where most documentation 
+        Blah blah blah example docstring. This is where most documentation
         for a function goes. If you mouse over the name of the function
         you'll see this description come up.
 
@@ -36,23 +54,24 @@ def highlight_terms(file_name):
 
         # Get all the contents from the file.
         content = source_stream.getvalue()
-        for line in content: 
+        for line in content:
             # code here for highlighting and boldening words
             line = line
 
     document = docx.Document(source_stream)
     source_stream.close()
-    
+
     target_stream = StringIO()
     document.save(target_stream)
 
 # Main ------------------------------------------------------------------------
 
+
 if __name__ == '__main__':
     # How can we pass in the file name to here?
     # highlight_terms(the_file)
-    
-    #* Here's how we can take command line arguments
+
+    # * Here's how we can take command line arguments
     # From the command line/terminal, you can run this script with
     # python highlight_search_terms.py path/to/file/here
     #
@@ -66,7 +85,8 @@ if __name__ == '__main__':
     # 2.    sys (and "os") is a library with a bunch of random system stuff
     #       the most important things are sys.argv and sys.exit
     #       sys.argv is a list all arguments passed in on the command line
-    #       NOTE: sys.argv[0] is always the name of the script, so "highlight_search_terms.py"
+    # NOTE: sys.argv[0] is always the name of the script, so
+    # "highlight_search_terms.py"
 
     if len(sys.argv) < 2:
         # If the only argument passed in was the script name
